a1a3ca7cec3584957de4eb99d4b110c15991a713
Siavash <Siavash_N@outlook.com>
2019-12-13 19:45:32 -0500

    More images -- Curved corners -- Search table feature

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,8 @@
 function onLoginLoaded() {
+    $(".login").corner("7px");
     $(".bg").fadeTo(2000, .4);
+}
+function onItemsLoaded(params) {
+    $(".header").corner("bottom 7px");
+    $(".formBody").corner("5px");
 }
